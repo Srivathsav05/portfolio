5740e4b60dd4bae6539128e3ec40f1afc8901124
Vathsav <vathsavvaddepally@gmail.com>
2024-09-08 22:48:24 +0530

    Portfolio By using Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'home', component:HomeComponent},
+    {path:'education', component:EducationComponent},
+    {path:'projects', component:ProjectsComponent},
+    {path:'experience',component:ExperienceComponent},
+    
+];
